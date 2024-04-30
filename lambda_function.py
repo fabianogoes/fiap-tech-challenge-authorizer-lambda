c20911a162c24227b3ea4b300495a233899e9d3f
Fabiano Góes <fabianogoes@gmail.com>
2024-04-29 21:15:39 -0300

    Update lambda_function.py (#4)

--- lambda_function.py
+++ lambda_function.py
@@ -5,26 +5,41 @@ from jwt.exceptions import ExpiredSignatureError
 
 load_dotenv()
 SECRET = os.getenv("SECRET")
-VERSION = "2024-04-28-2"
+VERSION = "2024-04-29-1"
 
 def lambda_handler(event, context):
     print("*********** the event is: *************")
     print(VERSION)
     print(event)
 
-    token = event['authorizationToken']    
-    auth = 'Deny'
-    if is_valid(token):
-        auth = 'Allow'
+    headers = event["headers"]
+    print(headers)
 
-        token_decoded = jwt.decode(token, SECRET, algorithms=["HS256"])
-        user = token_decoded["user"]
-        email = token_decoded["email"]
-        print(f"user={user}, email={email}")
+    token =headers["authorizationtoken"]
+    print(f"authorizationtoken={token}")
+            
+    auth = False
+    if is_valid(token):
+        try:
+            print(f"SECRET={SECRET}")
+            token_decoded = jwt.decode(token, SECRET, algorithms=["HS256"])
+            print(token_decoded)
+            
+            user = token_decoded["user"]
+            email = token_decoded["email"]
+            cpf = token_decoded["sub"]
+            auth = True
+            
+            print(f"cpf={cpf}, user={user}, email={email}")
+        except ExpiredSignatureError:
+            print("Expired Token")
+        except:
+            print("Validation token error")
     else:
-        auth = 'Deny'
+        auth = 'false'
         
-    authResponse = { "principalId": user, "policyDocument": { "Version": "2012-10-17", "Statement": [{"Action": "execute-api:Invoke", "Resource": ["arn:aws:execute-api:us-east-1:758397526889:0no1axjaq1/*/*/*"], "Effect": auth}] }}
+    # authResponse = { "principalId": user, "policyDocument": { "Version": "2012-10-17", "Statement": [{"Action": "execute-api:Invoke", "Resource": ["arn:aws:execute-api:us-east-1:758397526889:0no1axjaq1/*/*/*"], "Effect": auth}] }}
+    authResponse = {"isAuthorized": auth}
     return authResponse
 
 
