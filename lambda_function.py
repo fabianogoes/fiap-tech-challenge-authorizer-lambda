05358c1150bf21ed51d40577d215c94323088e0f
Fabiano Góes <fabianogoes@gmail.com>
2024-08-26 08:43:33 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ from jwt.exceptions import ExpiredSignatureError
 
 load_dotenv()
 SECRET = os.getenv("SECRET")
-VERSION = "2024.5.8.2"
+VERSION = "2024.8.26.1"
 EFFECT_ALLOW = "Allow"
 EFFECT_DENY = "Deny"
 
